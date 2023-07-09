30a0835d41a79c8440dc17963859559a7dcabbbf
joe <joeho7576@gmail.com>
2023-07-09 16:35:55 +0330

    add all the materials used in this book :D

---
+++ Part 2/Practices/Chapter 13/excel_notes.py
@@ -0,0 +1,19 @@
+# openpyxl -> work with excel and stuff
+# 2.6.2 is the older version used in this book :(
+
+'''
+As a quick review, here’s a rundown of all the functions, methods, and data types involved in reading a cell out of a spreadsheet file:
+
+- Import the openpyxl module.
+- Call the openpyxl.load_workbook() function.
+- Get a Workbook object.
+- Use the active or sheetnames attributes.
+- Get a Worksheet object.
+- Use indexing or the cell() sheet method with row and column keyword arguments.
+- Get a Cell object.
+- Read the Cell object’s value attribute.
+'''
+
+# A census tract is simply a geographic area defined for the purposes of the census.
+# census: An official count or survey of a population, typically recording various details of individuals.
+
